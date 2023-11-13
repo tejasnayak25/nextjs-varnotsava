037c695a4dc17681116ce58515ecf0f0eb0ac4af
Tejas Nayak <arpithanayak900@gmail.com>
2023-11-13 20:19:15 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import { NovusBase } from "@/lib/models"
 import { Form } from "@/lib/form"
-import Lamp from "@/public/images/lamp.png"
-import Flow from "@/public/images/flow.png"
+// import Lamp from "@/public/images/lamp.png"
+// import Flow from "@/public/images/flow.png"
 import localFont from 'next/font/local'
+
+const Lamp = "https://th.bing.com/th/id/OIP.mFuXoMcAqbvUwjDjwTkFHAHaHa?pid=ImgDet&rs=1";
+const Flow = "https://th.bing.com/th/id/OIP.mFuXoMcAqbvUwjDjwTkFHAHaHa?pid=ImgDet&rs=1";
 
 // const surt = localFont({
 //   src: [
