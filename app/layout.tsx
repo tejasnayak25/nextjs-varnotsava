13ecaa553806084b063ba002fc717e6829590f20
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-15 20:29:31 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from "./header.tsx"
+import Header from "./header"
 
 const inter = Inter({ subsets: ['latin'] })
 
