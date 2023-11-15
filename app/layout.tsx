a39d3bae8425e4968bc4b18650f90e541b16b4f2
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-15 20:27:55 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        {Header}
+        <Header/>
         {children}
       </body>
     </html>
