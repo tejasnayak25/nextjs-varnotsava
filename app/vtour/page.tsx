9e310baa242f76989f7337af4816f9541d0f074d
Tejas Nayak <arpithanayak900@gmail.com>
2023-11-18 16:24:14 +0530

    Update page.tsx

--- app/vtour/page.tsx
+++ app/vtour/page.tsx
@@ -33,6 +33,6 @@ function getImages() {
 
 export default async function Home() {
   return (
-    <iframe className="w-full h-full" src="https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2fsearch%2fShri%2bMadhwa%2bVadiraja%2bInstitute%2bof%2bTechnology%2band%2bManagement,%2bVishwothama%2bNagar,%2bBantakal,%2bKarnataka%2f%4013.2542477,74.78500508,59.52282685a,0d,60y,196.68825114h,100.2683794t,0r%2fdata%3dCigiJgokCXHHnCFB_UxAEXLHnCFB_UzAGVGqFcKZqkhAIZb28OT2sErAIjAKLEFGMVFpcE9nSV93NHQ2cER0dzFhbEJhT2dEWDJ5UHVSY1o2WkpvdU5pbG5OEAU"></iframe>
+    <iframe className="w-full h-full" src="https://earth.google.com/earth/d/1nGe6c8MloSJxSYvRdFDjQbX8ptQCId5_"></iframe>
   )
 }
