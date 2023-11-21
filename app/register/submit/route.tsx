f5e9ca446b3b41e24fb75acb9aa09a6b865d42d4
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-21 20:05:42 +0530

    Update

--- app/register/submit/route.tsx
+++ app/register/submit/route.tsx
@@ -19,14 +19,20 @@ export async function POST(req: NextRequest, res:NextResponse) {
     });
 
     await send(data.email, `
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css" integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <div class="w-100 d-flex justify-content-center bg-black text-white">
-            <p class=" w-100 text-center p-5 bold">Varnotsava - 2K23</p>
-            <p class=" mb-3">We have received a registration request for the ${data.event} event</p>
-            <p class=" mb-6">Click the button below to confirm your registration</p>
-            <a href="/register/confirm?id=${data.team_name}_${data.event}" class=" btn btn-primary mb-6">Confirm Registration</a>
-            <p class="text-muted">Powered by FeatureX</p>
-        </div>
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css" integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            </head>
+            <body>
+                <div class="w-100 d-flex justify-content-center bg-black text-white">
+                    <p class=" w-100 text-center p-5 bold">Varnotsava - 2K23</p>
+                    <p class=" mb-3">We have received a registration request for the ${data.event} event</p>
+                    <p class=" mb-6">Click the button below to confirm your registration</p>
+                    <a href="https://nextjs-varnotsava.vercel.app/register/confirm?id=${data.team_name}_${data.event}" class=" btn btn-primary mb-6">Confirm Registration</a>
+                    <p class="text-muted">Powered by FeatureX</p>
+                </div>
+            </body>
+        </html>
     `)
     
     return NextResponse.json({"data":"Confirmation mail has been sent to your email."});
