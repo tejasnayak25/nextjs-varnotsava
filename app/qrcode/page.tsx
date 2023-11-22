fd9cbc479924c88772c545dc21dbb4ad6eb11e0d
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-22 11:25:12 +0530

    Update

---
+++ app/qrcode/page.tsx
@@ -0,0 +1,25 @@
+import { collection } from '@/lib/firebase-admin';
+import { QRCodes, Registration } from '@/lib/models';
+import { useRouter } from 'next/navigation';
+import { put } from '@/lib/gform';
+import { send } from '@/lib/sendmail';
+import { toDataURL } from "qrcode";
+import { NextResponse } from 'next/server';
+
+let registration = collection(Registration);
+let qrcodes = collection(QRCodes);
+
+export default async function Home({
+    searchParams,
+  }: {
+    searchParams: { [key: string]: string };
+  }) {
+    let id = searchParams["id"];
+    
+    let row = await qrcodes.doc(id).get();
+    let data = row.data();
+    let url = data ? data.url : "";
+
+    return NextResponse.redirect(url);
+}
+  
--- app/register/confirm/page.tsx
+++ app/register/confirm/page.tsx
@@ -1,19 +1,12 @@
 import { collection } from '@/lib/firebase-admin';
-import { Registration } from '@/lib/models';
+import { QRCodes, Registration } from '@/lib/models';
 import { useRouter } from 'next/navigation';
 import { put } from '@/lib/gform';
 import { send } from '@/lib/sendmail';
-import { toFile } from "qrcode";
-import path from 'path';
-import * as fs from "fs";
-import getConfig from 'next/config';
-
-
-const serverPath = (staticFilePath: string) => {
-  return path.join(getConfig().serverRuntimeConfig.PROJECT_ROOT, staticFilePath)
-}
+import { toDataURL } from "qrcode";
 
 let registration = collection(Registration);
+let qrcodes = collection(QRCodes);
 
 export default async function Home({
     searchParams,
@@ -33,13 +26,13 @@ export default async function Home({
                     margin : 7,
                     width : 175
                 };
-
-                if(!fs.existsSync("./qrcodes")) {
-                   fs.mkdirSync("./qrcodes") ;
-                }
                 
-                let url = await toFile(`./qrcodes/${id}.png`, newid);
-                console.log(url);
+                let url = await toDataURL(newid, qrOption);
+                
+                qrcodes.doc(newid).set({
+                    url: url
+                });
+                
                 send(info.email, `
                 <html>
                     <head>
@@ -48,7 +41,7 @@ export default async function Home({
                     <body>
                         <div class="w-100 d-flex justify-content-center bg-black text-white">
                             <p class=" w-100 text-center p-5 bold">Varnotsava - 2K23</p>
-                            <img src="https://nextjs-varnotsava.vercel.app/qrcodes/${id}.png" alt="" className="w-100 mb-10 lg:mb-0" />
+                            <img src="https://nextjs-varnotsava.vercel.app/qrcode?id=${newid}" alt="" className="w-100 mb-10 lg:mb-0" />
                             <p class=" mb-3">Congratulations!! You have successfully registered for the ${info.event} event</p>
                             <p class=" mb-6">Use this QR Code for further processes.</p>
                             <a href="https://nextjs-varnotsava.vercel.app/register/confirm?id=${data.team_name}_${data.event}" class=" btn btn-primary mb-6">Cancel Registration</a><br>
