ccccd7ca67d1ad8e21795502f76a8f9f8246f9e3
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-22 19:30:12 +0530

    Update

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,47 +1,10 @@
-import { collection, fbaseAdmin } from "@/lib/firebase-admin";
-import { Admin } from "@/lib/models";
-import { send } from "@/lib/sendmail";
-// components/LoginButton.js
-import { useEffect, useState } from 'react';
+// pages/index.js or your component file
+import LoginButton from '@/app/components/LoginButton';
 
-let admincollec = collection(Admin);
-
-export default function LoginButton() {
-    const [isClicked, setIsClicked] = useState(false);
-
-    async function sendCred() {
-        let credentials = await admincollec.doc("credentials").get();
-        let data = credentials.data() || {};
-
-        let email = data["email"] || "";
-        let pass = email + "_Admin_" + Date.now();
-
-        console.log(email);
-        
-        admincollec.doc("credentials").update({
-            pass: String(pass)
-        });
-        
-        await send(email, `
-        <p>Varnotsava Admin: <a href="https://nextjs-varnotsava.vercel.app/admin?id=${pass}">Login</a></p>
-        `);
-        return undefined;
-    }
-
-    const handleClick = async () => {
-        setIsClicked(true);
-        await sendCred();
-    };
-
-    useEffect(() => {
-        if (isClicked) {
-            // Perform any post-click logic here
-        }
-    }, [isClicked]);
-
+export default function Login() {
     return (
-        <button onClick={handleClick} className="btn bg-pink-red text-slate-900 hover:text-white">
-            {isClicked ? 'Credentials Sent' : 'Send Credentials'}
-        </button>
+        <div className="p-5 flex justify-center items-center">
+            <LoginButton />
+        </div>
     );
 }
