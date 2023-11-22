b544ba900b56bf580455734e9963029589eea5d8
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-22 21:51:23 +0530

    Update

--- app/carousel.tsx
+++ app/carousel.tsx
@@ -8,8 +8,8 @@ import 'slick-carousel/slick/slick-theme.css';
 const Carousel = () => {
   useEffect(() => {
     // Importing react-slick styles on the client side
-    import('slick-carousel/slick/slick.css');
-    import('slick-carousel/slick/slick-theme.css');
+    // import('slick-carousel/slick/slick.css');
+    // import('slick-carousel/slick/slick-theme.css');
   }, []);
 
   const settings = {
