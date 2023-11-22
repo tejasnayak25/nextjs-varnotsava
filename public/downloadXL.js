ff2206c5aaf3c6da0310a2c842de3f96fd648f89
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-22 21:27:48 +0530

    Update

--- public/downloadXL.js
+++ public/downloadXL.js
@@ -1,8 +1,31 @@
 document.getElementById("downloadXL").onclick = () => {
-    $("#data-table").table2excel({
-        exclude: ".noExl", // Exclude elements with the class 'noExl'
-        name: "Registration",
-        filename: "excelFile", // Specify the filename
-        fileext: ".xls" // Specify the file extension
-      });
+    exportTableToExcel("data-table", "Varnotsava - Registration")
 }
+
+function exportTableToExcel(tableId, filename = 'excelFile') {
+    const table = document.getElementById(tableId);
+    const rows = table.querySelectorAll('tr');
+    const csvData = [];
+
+    // Iterate through rows and columns to build CSV data
+    for (const row of rows) {
+      const rowData = [];
+      const cols = row.querySelectorAll('td, th');
+      for (const col of cols) {
+        rowData.push(col.innerText);
+      }
+      csvData.push(rowData.join(','));
+    }
+
+    // Create a CSV file
+    const csvContent = 'data:text/csv;charset=utf-8,' + csvData.join('\n');
+    const encodedUri = encodeURI(csvContent);
+
+    // Create a link element and trigger a click to download the file
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', `${filename}.csv`);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
