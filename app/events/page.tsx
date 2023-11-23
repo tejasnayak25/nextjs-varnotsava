19fc14747bf841b58206fbc25f22d8f47d9d4328
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-23 11:08:03 +0530

    Update

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-16 px-5">
-            <p className=" text-lg lg:text-2xl relative w-full">Technical Event Rules: INTRA <span className="absolute right-3"><a download={"Event_Details.pdf"} href="https://docs.google.com/document/d/190y-uResmwuqF8VWinTZKGp9eGxFa4xrcHKxrvPNTz8/edit?usp=drive_link" className="btn bg-pink-red text-black hover:text-white">View PDF</a></span></p>
+            <p className=" text-lg lg:text-2xl relative w-full text-center">Technical Event Rules: INTER <span className="absolute right-3"><a download={"Event_Details.pdf"} href="https://docs.google.com/document/d/190y-uResmwuqF8VWinTZKGp9eGxFa4xrcHKxrvPNTz8/edit?usp=drive_link" className="btn bg-pink-red text-black hover:text-white">View PDF</a></span></p>
             <table className="table mb-10 w-full">
                 <thead>
                     <tr className="table-row">
@@ -139,7 +139,7 @@ export default function Home() {
                 </tbody>
             </table>
 
-            <p className=" text-lg lg:text-2xl">Technical Event Rules: INTER</p>
+            <p className=" text-lg lg:text-2xl">Technical Event Rules: INTRA</p>
             <table className="table mb-10">
                 <thead>
                     <tr className="table-row">
