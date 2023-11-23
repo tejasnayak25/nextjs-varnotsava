c565182d8827decdb28a0b80ac49f373b886cc5d
tj-likes-coding <contacttejas2005@gmail.com>
2023-11-23 05:54:33 +0530

    Update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,43 +4,61 @@ const AboutPage = () => {
   return (
     <div className="bg-black min-h-screen p-8">
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-bold mb-6">About Our Institution</h1>
+      <p className='text-lg lg:text-2xl font-bold w-full text-center mb-5'>About Us</p>
+      <p>Founded in 2010 by <strong>H. H. Shri Vishwavallabha Theertha Swamiji</strong> of Shri Sode Vadiraja Mutt – one of the 700 plus years old <em>Ashta Mutts </em>associated with the world famous Shri Krishna temple of Udupi in Karnataka – <strong>Shri MadhwaVadiraja Institute of Technology &amp; Management</strong> (SMVITM), situated at Bantakal in Udupi has carved a niche for itself in imparting quality engineering education in the coastal Karnataka region. Shri Swamiji started the institute with the sublime objective of providing <strong>quality higher education to the needy student community at affordable costs</strong>. The motto <em>Engineering your Career and Character with Care </em>well reflects the objectives and philosophy of the institute. Despite, being the youngest institute in the region, SMVITM has made remarkable strides in its journey towards excellence over the past five years and has emerged as the most promising engineering institute with its well-qualified and motivated faculty, state-of-the-art infrastructure and distinguished learning-centric facilities.</p>
+      <p>At present, SMVITM, with a total student intake of 480 per year, offers Bachelor of Engineering (B.E.) courses in Artificial Intelligence and Data Science, Artificial Intelligence in Machine Learning,  Civil Engineering, Computer Science &amp; Engineering Electronics &amp; Communication Engineering and Mechanical Engineering. Despite being located in a rural area, SMVITM ensures that its students are well trained in existing engineering practices and acquainted with the latest industrial trends.</p>
+      <p>The campus is conveniently located at Bantakal, which is about 6 km off the stretch of NH 66 that connects the coastal cities of Mangaluru (Mangalore) and Udupi. The campus is just 18 km away from Manipal, one of the well known educational hubs in South India and 12 km away from Udupi town. Udupi is well connected by road, rail and air to all the major cities across the country. The institute has well-furnished in-campus as well as off-campus hostels with all modern amenities, separately for boys and girls. The institute is affiliated to Visvesvaraya Technological University, Belagavi; approved by the All India Council for Technical Education, New Delhi and recognized by the Government of Karnataka. The institution is not accredited till now.</p>
+      <p>The accomplishments of SMVITM have already garnered well-deserved recognition and widespread praise from eminent personalities across the spectrum. Late Dr. A. P. J. Abdul Kalam, former President of India, having admired the vision and value addition to engineering education taking place at the institute, made a visit to the campus and appreciated the holistic education imparted at SMVITM and inspired the students in 2014. Shri A. S. Kiran Kumar, Chairman, ISRO; Dr. B. N. Suresh, Distinguished Scientist, ISRO; Justice Dr. M. Rama Jois and many more renowned scientists, technocrats and educationists have visited the institute to motivate and guide the students.</p>
+      <p>Owing to its exalted Vision, Mission and Objectives; commitment to the cause of education and remarkable accomplishments, SMVITM is consistently maintaining record admissions of more than 90 per cent of the intake for the last three years. Though the institution is still taking off, the achievements of its students, faculty and staff are many and commendable. The institute is growing fast, scaling greater heights by concentrating on excellence in education, research and promotion of placement &amp; entrepreneurship through industry-academia tie-ups and value added programs. The management and the academic administration have been steadfast in their commitment to make SMVITM one of the best engineering institutes in South India in the very near future.</p>
+      <p><strong><em>Some Milestones in the Journey towards Excellence:</em></strong></p>
+      <ul>
+      <li>Formation of External Technical Advisory Board comprising distinguished researchers and eminent professors from the USA in an attempt to create interdisciplinary Centers of Excellence and facilitating multi-faceted technological growth</li>
+      <li>Academic partnerships/MoU made with leading industries/organizations like SAP Labs, Bengaluru; CVC Pvt. Ltd., Bengaluru; Advanced Electronic Systems, Bengaluru; Vinayaka Engineering Works, Mangaluru; University of Agricultural &amp; Horticultural Sciences, Shivamogga; FE Designs, Bengaluru for training/internship/project work/continuing education programs for the students, faculty and staff</li>
+      </ul>
+      <ul>
+      <li>Consistently topping in University (VTU) examination results &#8211; 100percent results recorded in both first and second outgoing batches.</li>
+      </ul>
+      <ul>
+      <li>VTU recognized Research Centers and execution of externally funded research projects in the Departments of Basic Sciences</li>
+      <li>Establishment of R&amp;D Centre funded by KCTU, Dept. of Commerce &amp; Industry, Govt. of Karnataka on Technology Up-gradation of Cashew Processing Equipment in the Department of Mechanical Engineering</li>
+      <li>IT facility agreement with premier IT company Tata Consultancy Services (TCS)</li>
+      <li>Collaboration with CSIR, NAL and ISRO scientists for student training/outreach programs</li>
+      <li>Institutional merit-cum-means scholarships and other attractive scholarships/awards instituted by benevolent well-wishers, charitable trusts and Govt. Departments</li>
+      <li>Striking Placement Opportunities and Facilities<strong> &#8211; </strong>Fully furnished, spacious placement block to conduct all the processes related to campus placement drives</li>
+      </ul>
+      <ul>
+      <li>Enhanced company placements – Many students already placed in HP, TCS, Infosys, IBM, Mphasis, Robosoft, Cognizant, HCL, WIPRO, Celstream, CVC, Unisys, Diya Systems, Nile Stream, Vee Technologies, Ethnus, Indian Navy, Sri Dutt Technologies, Manipal Digital Systems, Manipal Technologies Ltd., Samundra, Telenetix, Sarayu, Manipal Dot Net, Axionet, Good Through Software, Novigo Solutions, Westline Constructions, Cruiseline Ship Management Services, Adani Power Group, Lancy Constructions, UST Global, Cigital India, Polycab Wires and many more companies.</li>
+      </ul>
+      <ul>
+      <li>Fully functional and recognized Testing &amp; Consultancy Cell in the Civil Engineering Department</li>
+      <li>KSCST &amp; VGST (TRIP), Govt. of Karnataka sponsored and promoted student project work having social impact</li>
+      <li>Effective pre-placement training and personality development programs on a regular basis</li>
+      <li>Grants under <em>Seed Money to Young Scientists for Research </em>(SMYSR) scheme of Govt. of Karnataka</li>
+      <li>Unique, industry promoted <em>Innovation Lab</em> in the E&amp;CE Department and <em>Creativity Center</em> in the Mechanical Engineering Dept. and <em>Internet of Things Club</em> in the CS&amp;E Dept. to identify, bring out, nurture and promote creative ideas/innovative thinking of budding engineers</li>
+      <li>Well-qualified and highly committed team of faculty and staff, satisfying all the statutory requirements with doctorates serving in all departments</li>
+      <li>Ongoing research activity: more than 27 research/review papers in well recognized national/international journals and more than 22 papers in the national/international conferences/symposiums (with in India as well as abroad) presented/published during the past one year; Many faculty members pursuing doctoral studies</li>
+      <li>Organization of state level, inter-collegiate, techno-cultural fests and Project Model Exhibition every year to recognize and reward talented students</li>
+      <li>Students encouraged and guided to participate and win district, state and national level championships and awards in co-curricular and extra-curricular competitions</li>
+      <li>ISTE sponsored faculty development programs</li>
+      <li>Regular organization of various technical/social service programmes under the banner of ISTE Student Chapter, NSS &amp; YRC Wings as well as Rotaract Club</li>
+      <li>Conducting science model and project exhibitions to recognize, nurture and motivate talented PUC students of the region every year</li>
+      <li>Organization of career guidance/pre-counseling programs to the local pre-university students as a value added service to society</li>
+      <li>Invited presentations by the faculty on international forums abroad</li>
+      </ul>
+      <p><strong> </strong></p>
+      <p><strong><em>Major Projects in the Pipeline:</em></strong></p>
+      <ul>
+      <li><em>Centre for Preservation of Ancient Manuscripts (CPAM</em>): An initiative of Prof. P R Mukund, Rochester Institute of Technology, USA</li>
+      <li><em>Adoption of Innanje Village for Socio-economic and Total Intellectual Development</em>: Enabling the villagers to use products of science &amp; technology useful for their daily lives by imparting required skill sets at their doorsteps</li>
+      <li>Institute promoted <em>Technology Incubation Center</em> at Udupi, facilitating budding entrepreneurs to come up with their self-sustainable start-ups</li>
+      <li>Promoting ambitious schemes/projects of Govt. of India such as <em>Start Up India; Stand Up India, Digital India </em></li>
+      <li>Developing Eco-friendly Green Campus in the near future by harnessing renewable energy resources to the fullest extent for efficient energy and water management</li>
+      <li><em>Find Your Roots</em>: a Techno-Cultural India-Heritage Camp for Students (10+2 level) from abroad – a   mixture of Yoga/meditation, cultural, spiritual, educational and technical activities.</li>
+      </ul>
 
-        <p className="text-white mb-4">
-          Welcome to our institution! We are dedicated to providing high-quality education and fostering an environment of
-          learning and growth. Our institution has a rich history and a commitment to excellence in all aspects of
-          academic and extracurricular activities.
-        </p>
-
-        <p className="text-white mb-4">
-          At our institution, we believe in the holistic development of our students. We provide a range of programs and
-          opportunities that enable students to explore their interests, develop critical thinking skills, and prepare
-          for a successful future.
-        </p>
-
-        <p className="text-white mb-4">
-          Our experienced faculty members are passionate about education and are dedicated to guiding students on their
-          educational journey. We strive to create a supportive and inclusive community where every student feels valued
-          and empowered.
-        </p>
-
-        <h2 className="text-2xl font-bold mt-8 mb-4">Our Mission</h2>
-
-        <p className="text-white mb-4">
-          The mission of our institution is to inspire and empower students to achieve their full potential. We are
-          committed to providing a nurturing and challenging educational environment that prepares students for
-          lifelong learning and success in a dynamic world.
-        </p>
-
-        <h2 className="text-2xl font-bold mt-8 mb-4">Vision Statement</h2>
-
-        <p className="text-white mb-4">
-          Our vision is to be a leading institution known for academic excellence, innovation, and the holistic
-          development of individuals. We aim to cultivate a community of learners who are equipped to make positive
-          contributions to society.
-        </p>
-
-        {/* Add more sections as needed */}
+     <div className='flex justify-center p-5 mt-10'>
+     <a className='btn bg-pink-red text-black hover:text-white w-56 rounded-full'>Read More</a>
+     </div>
       </div>
     </div>
   );
